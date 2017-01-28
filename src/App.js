6351532f83e264d5e99a35f0b274ce289ea69edf
michael_adrian <thomas.micheal.adrian.lucas@gmail.com>
2017-01-27 23:45:39 -0500

    p49 ES6 => function style

--- src/App.js
+++ src/App.js
@@ -24,26 +24,43 @@ class App extends Component {
     return (
       <div className="App">
 
-
         {
-          list.map(function(item) {
-                return (
-                  <div>
-                      <li>
-                        <a href={item.url} > {item.title}</a>
-                      </li>
-                      <li>{item.author}</li>
-                      <li>{item.num_components}</li>
-                      <li>{item.points}</li>
-                  </div>
-                ); //END RETURN BLOCK
-            }) //END FUNCTION //END LIST MAP
-        }
+          list.map( (item) =>
 
-      </div>//END APP
+                <div key={item.objectID}>
+                 <span>
+                   <a href={item.url}>{item.title}</a>
+                 </span>
+                 <span>{item.autor}</span>
+                 <span>{item.num_comments}</span>
+                 <span>{item.points}</span>
+               </div>
 
-    ); //END RETURN
+        ) //END FUNCTION
+      }
+
+    </div>//END APP
+
+    ); //END RETURN*/
   } //END RENDER
 } //END APP
 
 export default App;
+/*
+// first {} is where react injects data
+    {
+      list.map(function(item) {
+            return (
+              <div>
+                  <li>
+                    <a href={item.url} > {item.title}</a>
+                  </li>
+                  <li>{item.author}</li>
+                  <li>{item.num_components}</li>
+                  <li>{item.points}</li>
+              </div>
+            ); //END RETURN BLOCK
+        }) //END FUNCTION
+        //END LIST MAP
+    }
+    */
