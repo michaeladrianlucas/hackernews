d91b0cb994bb03bda452f7c1567c7b671118184c
michael_adrian <thomas.micheal.adrian.lucas@gmail.com>
2017-01-28 00:16:44 -0500

    setState for button

--- src/App.js
+++ src/App.js
@@ -22,9 +22,25 @@ class App extends Component {
     super(props);
 
     this.state = {
-      list: list,
+      list,
     };
+    this.onDismiss = this.onDismiss.bind(this);
   }
+
+  onDismiss(id) {
+    function isNotId(item){
+      return item.objectID !== id;
+    }
+    const updatedList = this.state.list.filter(isNotId);
+      this.setState({ list: updatedList });
+  }
+  /* or in ES6 aarrow function
+  onDismiss(id){
+  const isNotId = item => item.objectID !==id;
+  const updatedList = this.state.list.filter(isNotId);
+  this.setState({ list: updatedList });
+}
+  */
   render() {
     return (
       <div className="App">
@@ -34,9 +50,15 @@ class App extends Component {
                  <span>
                    <a href={item.url}>{item.title}</a>
                  </span>
-                 <span>{item.autor}</span>
+                 <span>{item.author}</span>
                  <span>{item.num_comments}</span>
                  <span>{item.points}</span>
+                 <span>
+                   <button onClick={ () => this.onDismiss(item.objectID) }
+                        type="button">
+                     Dismiss
+                   </button>
+                 </span>
                </div>
         ) //END FUNCTION
       }
